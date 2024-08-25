9bea6a93900c3b1e8d4740ac7a1a707d47012122
Tamryn <tamryn@kineticskunk.com>
2024-08-26 00:14:23 +0200

    change list: 	Modified pipeline 	Added generate_sas_token.sh

---
+++ generate_sas_token.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SAS_TOKEN=$(az storage blob generate-sas \
+    --account-name stazureblobstorageex \
+    --container-name files \
+    --name uploadedtextfile \
+    --permissions acdrw \
+    --expiry 2024-08-26T23:59:00Z \
+    --auth-mode login \
+    --as-user \
+    --full-uri \
+    --output tsv)
+
+# Export the SAS token as an environment variable
+echo "SAS_URL=${SAS_TOKEN}" >> $GITHUB_ENV
